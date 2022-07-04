4ce3aba2bc888232f5549cec2cadea6921fb750b
Heideenx3 <heiden.matias@gmail.com>
2022-07-03 23:52:52 -0300

    Se trabajó en el blog

--- sistema/animes/views.py
+++ sistema/animes/views.py
@@ -27,6 +27,7 @@ def nosotros(request):
 def animes(request):
     animes = Anime.objects.all()
     return render(request, 'animes/index.html', {'animes': animes})
+    
 @login_required
 def crear_anime(request):
     formulario = AnimeForm(request.POST or None, request.FILES or None)
